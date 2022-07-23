041258bd5c6c1c2201d14e5426c42b3f0a7fa2af
Davron <ergashevdavron948@gmail.com>
2022-07-23 16:23:18 +0300

    davron

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var yosh = +prompt ('yoshingizni kiriting')
+
+if (yosh <= 18){
+    alert('xali yoshsiz oqishingiz kerak')
+}
+else if(yosh <= 50){
+    alert('ishlashingiz kerak')
+}
+else if(yosh <= 59){
+    alert('yaqinda pensiyaga chiqasiz')
+}
+else if(yosh <= 150){
+    alert('pensionerga oxshaysiz xali tirik bolsangiz :)')
+}
+else{
+    alert('nimadir xato ketti')
+}
